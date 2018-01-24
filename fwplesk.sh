5ee597b9d0c64a059fb584ae778a088d92888ea9
Reiser89 <reiser089@gmail.com>
2018-01-24 20:40:41 +0100

    Create fwplesk.sh

---
+++ fwplesk.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# (c) Reiser, 2017-2018
+# Plesk allow ports
+
+# TCP allowed ports
+tcpports=( 20 21 22 25 53 80 106 110 113 143 443 465 587 953 990 993 995 3306 5432 8443 8447 8880 4190 6308 )
+
+# UDP allowed ports
+udpports=( 53 )
+
+for i in "${tcpports[@]}"
+do
+	firewall-cmd --permanent --add-port=$i/tcp
+done
+
+for i in "${udpports[@]}"
+do
+	firewall-cmd --permanent --add-port=$i/udp
+done
+
+firewall-cmd --reload
+echo "Following ports are open on your firewall:"
+firewall-cmd --list-ports
+echo "Done!"
